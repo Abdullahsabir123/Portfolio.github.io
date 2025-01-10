1cbb15b85564adbfa1c38c3b325c516e4766b276
Abdullahsabir123 <abdullahsabir558@gmail.com>
2025-01-10 22:08:15 +0500

    Completed

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,10 +6,8 @@ const Education = () => {
       <h1>Education</h1>
       <h3>Matriculation</h3>
       <p>Board of Secondary Education Karachi</p>
-      <p>Percentage: 71%</p>
       <h3>Intermediate</h3>
       <p>Board of Intermediate Education Karachi (Proccess)</p>
-      <p>Pending</p>
     </section>
   );
 };
